65777a35c2a18e6d54a18f3b81458b03d1752ac2
John Wang <johncwang@gmail.com>
2025-05-19 06:54:57 -0700

    enhance: `flag/cobrautil`: update `AddFlags()` with ability to handle flags without shorthand letter

--- flag/cobrautil/add_goflags_struct.go
+++ flag/cobrautil/add_goflags_struct.go
@@ -25,8 +25,8 @@ func AddFlags(cmd *cobra.Command, opts any) error {
 		field := typ.Field(i)
 		fieldVal := val.Field(i)
 
-		flagLong := field.Tag.Get("long")
-		flagShort := field.Tag.Get("short")
+		flagLong := strings.TrimSpace(field.Tag.Get("long"))
+		flagShort := strings.TrimSpace(field.Tag.Get("short"))
 		desc := field.Tag.Get("description")
 		defaultStr := field.Tag.Get("default")
 		required := strings.ToLower(strings.TrimSpace(field.Tag.Get("required")))
@@ -38,21 +38,33 @@ func AddFlags(cmd *cobra.Command, opts any) error {
 		switch field.Type.Kind() {
 		case reflect.String:
 			ptr := fieldVal.Addr().Interface().(*string)
-			cmd.Flags().StringVarP(ptr, flagLong, flagShort, defaultStr, desc)
+			if flagShort != "" {
+				cmd.Flags().StringVarP(ptr, flagLong, flagShort, defaultStr, desc)
+			} else {
+				cmd.Flags().StringVar(ptr, flagLong, defaultStr, desc)
+			}
 		case reflect.Int:
 			def := 0
 			if _, err := fmt.Sscanf(defaultStr, "%d", &def); err != nil {
 				return err
 			}
 			ptr := fieldVal.Addr().Interface().(*int)
-			cmd.Flags().IntVarP(ptr, flagLong, flagShort, def, desc)
+			if flagShort != "" {
+				cmd.Flags().IntVarP(ptr, flagLong, flagShort, def, desc)
+			} else {
+				cmd.Flags().IntVar(ptr, flagLong, def, desc)
+			}
 		case reflect.Bool:
 			def := false
 			if _, err := fmt.Sscanf(defaultStr, "%t", &def); err != nil {
 				return err
 			}
 			ptr := fieldVal.Addr().Interface().(*bool)
-			cmd.Flags().BoolVarP(ptr, flagLong, flagShort, def, desc)
+			if flagShort != "" {
+				cmd.Flags().BoolVarP(ptr, flagLong, flagShort, def, desc)
+			} else {
+				cmd.Flags().BoolVar(ptr, flagLong, def, desc)
+			}
 		case reflect.Slice:
 			if field.Type.Elem().Kind() == reflect.String {
 				defSlice := []string{}
@@ -60,7 +72,11 @@ func AddFlags(cmd *cobra.Command, opts any) error {
 					defSlice = stringsutil.SplitTrimSpace(defaultStr, ",", true)
 				}
 				ptr := fieldVal.Addr().Interface().(*[]string)
-				cmd.Flags().StringSliceVarP(ptr, flagLong, flagShort, defSlice, desc)
+				if flagShort != "" {
+					cmd.Flags().StringSliceVarP(ptr, flagLong, flagShort, defSlice, desc)
+				} else {
+					cmd.Flags().StringSliceVar(ptr, flagLong, defSlice, desc)
+				}
 			}
 		}
 		if required == "true" {
